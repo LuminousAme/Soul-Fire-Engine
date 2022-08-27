2763f5c5708a89030e44ef2976a81b5a507a0f55
LuminousAme <ame.gilham@ontariotechu.net>
2022-08-27 15:04:47 -0400

    glm added to pch and tested

--- SandBox/src/TestLayer.h
+++ SandBox/src/TestLayer.h
@@ -6,11 +6,20 @@ public:
 	TestLayer()
 		: Layer("App Test")
 	{
+		lastPos = glm::vec2(SoulFire::Input::GetMouseX(), SoulFire::Input::GetMouseY());
 	}
 
 	void Update() {
 		if (SoulFire::Input::GetKeyDown(SF_KEY_TAB)) {
-			SF_LOG_TRACE("Hello!");
+			glm::vec2 currentPos = glm::vec2(SoulFire::Input::GetMouseX(), SoulFire::Input::GetMouseY());
+
+			float dist = glm::distance(currentPos, lastPos);
+			SF_LOG_TRACE("Current position: {0}, {1}! Last Position: {2}, {3}!", currentPos.x, currentPos.y, lastPos.x, lastPos.y);
+			SF_LOG_TRACE("The mouse has moved {0} pixels!", dist);
+
+			lastPos = currentPos;
 		}
 	}
+private:
+	glm::vec2 lastPos = glm::vec2(0.0f, 0.0f);
 };
--- SoulFireEngine/src/SF_PCH.h
+++ SoulFireEngine/src/SF_PCH.h
@@ -15,6 +15,16 @@
 
 #include "SoulFire/Logger.h"
 
+//math
+#include "GLM/glm.hpp"
+#include "glm/common.hpp"
+#include <GLM/gtc/type_ptr.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+//allow use of experimental glm features
+#define GLM_ENABLE_EXPERIMENTAL
+#include "GLM/gtx/quaternion.hpp"
+#include "GLM/gtx/transform.hpp"
+
 #ifdef SF_PLATFORM_WINDOWS
 	#include <Windows.h>
 #endif
